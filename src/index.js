d76fd4fd18232e55e7902dda16e070d61798c077
Aryan Singh <aryansinghidm@gmail.com>
2025-06-20 14:13:09 +0530

    error and response api

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import mongoose, { connect } from "mongoose";
 import { DB_NAME } from "./constants.js";
 import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})    
+.catch((err) =>{
+    console.log("Error connecting to the database", err);
+    throw err;
+});
 
 
 
